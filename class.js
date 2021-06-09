8011cf1fcca7202c8a5f2f073c6465f8e0b4f0dd
David Jensen <tsourdox@icloud.com>
2021-06-09 15:55:26 +0200

    Added some class code

---
+++ class.js
@@ -0,0 +1,12 @@
+class Person {
+    constructor(name, age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    setAddress(address) {
+        this.address = address
+    }
+}
+
+const person = new Person('David', false);
